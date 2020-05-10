01f707da8a6c08740cfb25861c5293a15480df97
chrismantinsdev <chris.mantins.dev@gmail.com>
2020-05-10 19:46:57 -0300

    initial commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+const appointmentRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentRouter.post('/', (request, response) => {
+  try {
+    const { provider, date } = request.body;
+
+    const parsedDate = startOfHour(parseISO(date));
+
+    const findAppointmentInSameDate = appointmentsRepository.findByDate(
+      parsedDate,
+    );
+
+    if (findAppointmentInSameDate)
+      return response
+        .status(400)
+        .json({ message: 'This appointment is already booked' });
+
+    const appointment = appointmentsRepository.create(provider, parsedDate);
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentRouter;
