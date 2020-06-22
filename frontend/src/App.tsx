ce2c38a6fb8858ff6c8f0987dff269ad70c2be51
Christian <br>
2020-06-22 11:06:55 -0300

    feat: add handle error on form and add tooltiplayout[

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 import SingIn from './pages/SingIn';
-import SingUp from './pages/SingUp';
+// import SingUp from './pages/SingUp';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
     <>
-      <SingUp />
-      {/* <SingIn /> */}
+      {/* <SingUp /> */}
+      <SingIn />
       <GlobalStyle />
     </>
   );
--- frontend/src/pages/SingIn/index.tsx
+++ frontend/src/pages/SingIn/index.tsx
@@ -1,41 +1,68 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import LogoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={LogoImg} alt="GoBarber" />
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-      <form>
-        <h1>Faça seu logon</h1>
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
+      await schema.validate(data, { abortEarly: false });
+    } catch (error) {
+      console.log('Error: ', error);
 
-        <Input name="email" icon={FiMail} placeholder="Email" />
+      const errors = getValidationErrors(error);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
+  return (
+    <Container>
+      <Content>
+        <img src={LogoImg} alt="GoBarber" />
 
-        <Button type="submit">Entrar</Button>
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
 
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
+          <Input name="email" icon={FiMail} placeholder="Email" />
 
-      <a href="login">
-        <FiLogIn />
-        Criar conta
-      </a>
-    </Content>
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
-    <Background />
-  </Container>
-);
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+
+        <a href="login">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
