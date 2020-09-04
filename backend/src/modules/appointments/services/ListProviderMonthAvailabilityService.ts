439b1b956f0a1801fce201f14e33ec7951e47b3e
Christian <br>
2020-09-04 17:26:51 -0300

    feat: list providers date availability

--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,9 +1,10 @@
 import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 
-// import User from '@modules/users/infra/typeorm/entities/User';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
-  user_id: string;
+  provider_id: string;
   month: number;
   year: number;
 }
@@ -15,15 +16,43 @@ type IResponse = Array<{
 
 @injectable()
 class ListProviderMonthAvailabilityService {
-  constructor() {}
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
 
-  public async execute({ user_id, month, year }: IRequest): Promise<IResponse> {
-    return [
+  public async execute({
+    provider_id,
+    month,
+    year,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
       {
-        day: 1,
-        available: false,
+        provider_id,
+        year,
+        month,
       },
-    ];
+    );
+
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
 
